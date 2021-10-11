3f9cb0a9b213010380f0d2a1d7f495a939d8858c
tarxvf <git@tarxvf.tech>
2021-10-10 22:45:00 -0400

    this is all wrong, abort mission

--- m17/network.py
+++ m17/network.py
@@ -52,10 +52,11 @@ class n7tae_reflector_base:
         #recvq is packets received from the socket
         #sendq is packets to be sent out from our service
 
-    def add_connection(self, call, conn, prot):
+    def add_connection(self, call, my_channel, conn, prot):
         new_connection = dattr({
                 call: call,
                 conn: conn,
+                chan: my_channel,
                 prot: prot
                 })
         self.connections[conn] = new_connection
@@ -67,57 +68,69 @@ class n7tae_reflector_base:
         for call in self.connections:
             self.connections[call].prot.check_and_prune_if_dead()
 
-class n7tae_reflector_protocol:
+
+class n7tae_reflector_protocol_base:
     """
 
-    Requires an M17 callsign, 
-    a peer (udpconn) which is "0.0.0.0" and a port when a server, and a parent (service)
+    Requires 
+    * an M17 callsign, 
+    * a peer which is "0.0.0.0" and a port when a server, or the remote host and port when a client; 
+    * a parent (service) that is reponsible for the actual application using the reflector protocol, and probably handles the concurrency necessary
+    * a mode string, either "server" or "client"
+
     the service parameter currently needs to implement:
-        service.add_connection(call, conn, protocol) (where protocol is an instance of n7tae_reflector_protocol, so pass self)
+        service.add_connection(call, chan, conn, protocol) (where protocol is an instance of n7tae_reflector_protocol, so pass self)
         service.del_connection(conn) 
     The service should also call status check methods of this class as appropriate:
         check_connection
 
-    Can be subclassed to override send() for other protocols i guess
     """
-    def __init__(self, mycallsign, udpconn, service, mode="client"):
-        self.mycallsign=mycallsign
+    def __init__(self, mycallsign, peer, service, mode):
+        host,port = peer
+        self.mode = mode
+        if mode == "server":
+            #bind directly to specified port and wait for connections to this reflector
+            self.listenconn = peer
+            self.peer = None
+        else:# mode == "client"
+            self.listenconn = ("0.0.0.0", 17010)
+            self.peer = peer
+
+            #bind to a port of my choosing and connect out to remote reflector
+
+        self.mycallsign = mycallsign
         self.mycall_b = bytes(m17.address.Address(callsign=self.mycallsign))
-        self.udpconn = udpconn
         self.service = service #parent, usually a reflector or client
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(udpconn)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(self.listenconn)
         self.sock.setblocking(False)
 
+    def close(self):
+        self.sock.close()
 
-        if mode=="server":
-            ...
-
-    def connect_out(self ):
-        data = b"CONN" + self.mycall_b + self.module.encode("ascii")
-        self.send(data )
+    def connect(self, module="A"):
+        data = b"CONN" + self.mycall_b + module.encode("ascii")
+        self.send(data)
 
     def disco(self):
-        printf("DISC")
+        print("DISC")
         data = b"DISC" + self.mycall_b 
         self.send(data)
 
-    def connect_in(self, pkt):
+    def connect_in(self, pkt, peer):
         """
         pkt must not have the front magic bytes (4 bytes)
         so make sure to strip them (i.e. pass in pkt[4:] )
         """
-        print("Connection from ", udpconn);
-        print("pkt:", pkt);
         callsign_40 = pkt[:6]
         my_channel = chr(pkt[6])
-        print(callsign_40)
 
         addr = int.from_bytes(callsign_40, 'big')
-        print(addr)
         theircall = m17.address.Address(addr=addr)
-        print("from: ", theircall, " for my module ",my_channel)
-        ...
+        # print("from: ", theircall, " for my module ",my_channel)
+        return self.service.add_connection(theircall, my_channel, peer, self)
 
     def handle_disc( pkt):
         """
@@ -129,15 +142,29 @@ class n7tae_reflector_protocol:
         theircall = m17.address.Address(addr=addr)
         ...
 
-    def pong(self):
+    def pong(self, peer=None):
+        if peer is None and self.peer is not None:
+            peer = self.peer
         data = b"PONG" + self.mycall_b 
-        self.send(data)
+        self.send(data, peer)
         self.last_send_pongtime = time.time()
 
-    def ping(self):
+    def ping(self, peer=None):
+        if peer is None and self.peer is not None:
+            peer = self.peer
         data = b"PING" + self.mycall_b 
-        self.send(data)
+        self.send(data, peer)
         self.last_send_pingtime = time.time()
+    def ack(self, peer=None):
+        if peer is None and self.peer is not None:
+            peer = self.peer
+        data = b"ACKN"
+        self.send(data, peer)
+    def nack(self, peer=None):
+        if peer is None and self.peer is not None:
+            peer = self.peer
+        data = b"NACK"
+        self.send(data, peer)
 
     def check_and_prune_if_dead(self):
         # self.ping()
@@ -145,35 +172,46 @@ class n7tae_reflector_protocol:
         # self.service.del_connection(call)
         pass
 
-    def send(self,  pkt):
-        print("SEND:", pkt)
-        self.sock.sendto(pkt, self.udpconn)
+    def send(self,  pkt, peer=None):
+        if peer is None and self.peer is not None:
+            peer = self.peer
+        print("SEND:", peer, pkt)
+        self.sock.sendto(pkt, peer)
 
     def recv(self):
-        bs, clientconn = self.sock.recvfrom( 1500 ) 
+        try:
+            bs, clientconn = self.sock.recvfrom( 1500 ) 
+        except BlockingIOError as e:
+            return None
         return self.handle(bs, clientconn)
+
 
     def handle(self, pkt, from_conn):
         print("RECV:", pkt)
         if pkt.startswith(b"PING"):
             self.last_recv_pingtime = time.time()
-            self.pong()
-        if pkt.startswith(b"PONG"):
+            self.pong(from_conn)
+        elif pkt.startswith(b"PONG"):
             self.last_recv_pongtime = time.time()
             # self.pong()
         elif pkt.startswith(b"ACKN"):
+            
             pass
         elif pkt.startswith(b"NACK"):
-            self.disco()
+            # self.disco()
             #do more than this, like disco, reconnect, etc
 
             raise(Exception("Refused by reflector"))
         elif pkt.startswith(b"CONN"):
-            return self.connect_in( pkt[4:])
-        elif pkt.startswith(b"DISC"):
-            return self.handle_disc( pkt[4:])
+            if self.connect_in( pkt[4:], from_conn):
+                self.ack(from_conn)
+            else:
+                self.nack(from_conn)
+        # elif pkt.startswith(b"DISC"):
+            # return self.handle_disc( pkt[4:])
         else:
-            assert pkt[:4] == b"M17 "
+            print("unhandled")
+            # assert pkt[:4] == b"M17 "
             ...
 
 
@@ -216,6 +254,7 @@ class simple_n7tae_reflector(n7tae_reflector_base):
         """
         """
         srv_conn = (host, port)
+        # sock = udp_non_blocking_server(srv_conn)
         prot = n7tae_reflector_protocol(mycall, srv_conn, service=self, mode="server")
         while 1:
             try:
@@ -229,7 +268,7 @@ class simple_n7tae_reflector(n7tae_reflector_base):
             if not sendq.empty():
                 data,udpconn= sendq.get_nowait()
                 print("SEND", data,udpconn)
-                prot.send( data, udpconn, sock)
+                prot.send( data, udpconn)
             time.sleep(.000001)
 
     def reflector(self):
@@ -240,4 +279,37 @@ class simple_n7tae_reflector(n7tae_reflector_base):
                     continue
                 self.connections[conn].prot.send(pkt)
 
+class bare_service:
+    def __init__(self):
+        pass
+    def add_connection(self, callsign, my_channel, peer, protocol):
+        print("added", callsign, my_channel, peer, protocol)
+        return True
 
+class ReflectorProtocolTests(unittest.TestCase):
+    def testPingPong(self):
+        print("pingpong test")
+        a = n7tae_reflector_protocol_base("1",("0.0.0.0",17000), None,mode="server")
+        b = n7tae_reflector_protocol_base("2",("127.0.0.1",17000), None,mode="client")
+        b.ping() #send ping
+        a.recv() #a gets ping, pongs
+        b.recv() #b gets pong
+        a.close()
+        b.close()
+
+
+    def testConnect(self):
+        print("connect test")
+        asrv = bare_service()
+        bsrv = bare_service()
+        a = n7tae_reflector_protocol_base("1", ("0.0.0.0",17000), asrv, mode="server")
+        b = n7tae_reflector_protocol_base("2", ("127.0.0.1",17000), bsrv, mode="client")
+        # b.connect("1", ("127.0.0.1", 17000), "Z")
+        b.connect("Z")
+        # self.assertTrue(b.is_connected("1","Z"))
+        # can't really have state without concurrency, really :/
+        a.recv() 
+        b.recv()
+        a.close()
+        b.close()
+        print("end connect")
